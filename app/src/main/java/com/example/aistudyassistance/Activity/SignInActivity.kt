7a4c95cbc532b5145604ed91ca1608bff7c50330
Yogii9071 <yogeshtheboy17@gmail.com>
2026-02-24 13:18:07 +0530

    Added login Page

---
+++ app/src/main/java/com/example/aistudyassistance/Activity/SignInActivity.kt
@@ -0,0 +1,17 @@
+package com.example.aistudyassistance.Activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.aistudyassistance.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_in)
+
+    }
+}
